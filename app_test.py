9579358548ee747b9ce952492fc3e8fd370947be
Anthony Duncalf <ajd224@ic.ac.uk>
2024-10-21 15:59:39 +0100

    trial

--- app_test.py
+++ app_test.py
@@ -1,14 +1,17 @@
 import pytest
 from app import app  # Make sure this matches your app's import path
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+
 def test_knows_about_dinosaurs(client):
     response = client.get("/query?q=dinosaurs")
     assert response.data.decode() == "Dinosaurs ruled the Earth 200 million years ago"
+
 
 def test_does_not_know_about_asteroids(client):
     response = client.get("/query?q=asteroids")

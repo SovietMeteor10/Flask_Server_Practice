32bda37379d1dba4b805d72b74e4966608decdfe
Anthony Duncalf <ajd224@ic.ac.uk>
2024-10-28 14:27:10 +0000

    name

--- app.py
+++ app.py
@@ -28,6 +28,6 @@ def process_query(query_string):
         return "Dinosaurs ruled the Earth 200 million years ago"
     if query_string == "asteroids":
         return "Unknown"
-    if "your name" in query:
+    if "your name" in query_string:
         return "Team_Wun"
     return "Query not recognised"

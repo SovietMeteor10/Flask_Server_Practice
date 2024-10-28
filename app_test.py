62ecf4c1257de48f1277c19c6df0a9efad1c66cd
Anthony Duncalf <ajd224@ic.ac.uk>
2024-10-28 14:39:48 +0000

    name

--- app_test.py
+++ app_test.py
@@ -11,4 +11,4 @@ def test_does_not_know_about_asteroids():
 
 
 def test_for_name():
-    assert process_query("What is your name?") == "Team Wun"
+    assert process_query("What is your name?") == "Team_Wun"
